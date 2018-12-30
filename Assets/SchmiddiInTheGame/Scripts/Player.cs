3aa1e6bd5dc0d049f915d20c2f030c23ef3db566
Marcel Schmidt <marcel.schmidt1987@outlook.com>
2018-12-30 15:59:16 +0100

    Added Player Script
    Added Input Control and public float Variable to set moveSpeed in Inspector.

--- Assets/SchmiddiInTheGame/Scripts/Player.cs
+++ Assets/SchmiddiInTheGame/Scripts/Player.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float moveSpeed = 1f;
+
+    private void FixedUpdate () 
+    {
+        float step = roundToPixelGrid(moveSpeed * Time.deltaTime);
+
+        Vector3 change = new Vector3();
+
+        if (Input.GetKey(KeyCode.D))
+            change.x = step;
+        else if (Input.GetKey(KeyCode.A))
+            change.x = -step;
+        else if (Input.GetKey(KeyCode.W))
+            change.y = step;
+        else if (Input.GetKey(KeyCode.S))
+            change.y = -step;
+        transform.position += change;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    /// <summary>
+    /// Größe eines PixelArt-Pixels in Unity-Einheiten.
+    /// </summary>
+    private static float pixelFrac = 1f / 32f; //32 = Pixels per Unit
+
+    /// <summary>
+    /// Runde auf PixelArt-Pixel.
+    /// </summary>
+    /// <param name="f">Zahl, die gerundet werden soll.</param>
+    /// <returns>f, eingerastet im PixelArt-Raster.</returns>
+    private float roundToPixelGrid(float f)
+    {
+        return Mathf.Ceil(f / pixelFrac) * pixelFrac;
+    }
 }
